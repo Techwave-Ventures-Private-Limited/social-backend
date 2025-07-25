0b781f067e3e79c23cba9bb780e0621f2ee1cc47
legion <bagulshubham72@gmail.com>
2025-07-25 18:11:49 +0530

    Story fix

--- route/userRoute.js
+++ route/userRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const {getUser, updateUser, uploadProfileImage, sendForgotPasswordEmail, verifyForgotPasswordOtp, changePassword} = require("../controller/UserController");
-const {createStory, getFollowingStories} = require("../controller/StoryController");
+const {createStory, getFollowingStories, getCurrentStory} = require("../controller/StoryController");
 const {auth}  = require("../middleware/authMiddleware");
 const { followUser } = require("../controller/FollowController");
 
@@ -12,6 +12,7 @@ router.post("/follow", auth, followUser);
 router.post("/uploadProfileImage", auth, uploadProfileImage);
 router.post("/upload/story", auth, createStory);
 router.get("/story", auth, getFollowingStories);
+router.get("/story/self", auth, getCurrentStory);
 router.post("/forgotPassword", sendForgotPasswordEmail);
 router.post("/verifyForgotPassword", verifyForgotPasswordOtp);
 router.post("/changePassword", changePassword);
