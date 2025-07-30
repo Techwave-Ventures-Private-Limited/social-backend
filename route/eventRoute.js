844bf9d9bb5b5f62419d7f304b5125f22d119d12
kandalkardattu <dattukandalkar143@gmail.com>
2025-07-30 11:21:33 +0530

    added new api

--- route/eventRoute.js
+++ route/eventRoute.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const { createEvent, createTicket, getEvent } = require("../controller/EventController");
+const { createEvent, createTicket, getEvent, getAllEvents, bookTicket,getUserBookedEvents } = require("../controller/EventController");
 const router = express.Router();
 const {auth} = require("../middleware/authMiddleware");
 
 router.post("/createEvent", auth, createEvent);
 router.post("/createTicket", auth,createTicket);
+router.get("/getUserBookedEvents", auth, getUserBookedEvents);
+router.post("/bookTicket",  bookTicket);
+router.get("/getAllEvents", getAllEvents);
 router.get("/:eventId", getEvent);
+
 
 module.exports = router;
