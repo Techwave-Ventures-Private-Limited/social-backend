45a5fb4f7e80de4ff22612d6530fc01a0b76ef0e
kandalkardattu <dattukandalkar143@gmail.com>
2025-07-31 13:20:39 +0530

    template package change

--- route/eventRoute.js
+++ route/eventRoute.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const { createEvent, createTicket, getEvent, getAllEvents, bookTicket, getUserBookedEvents, generateEventTicket } = require("../controller/EventController");
+const { createEvent, createTicket, getEvent, getAllEvents, bookTicket, getUserBookedEvents, generateEventTicketPDF } = require("../controller/EventController");
 const router = express.Router();
 const {auth} = require("../middleware/authMiddleware");
 
 router.post("/createEvent", auth, createEvent);
 router.post("/createTicket", auth, createTicket);
 router.get("/getUserBookedEvents", auth, getUserBookedEvents);
+router.get("/:eventId", getEvent);
 router.post("/bookTicket", bookTicket);
 router.get("/getAllEvents", getAllEvents);
-router.get("/ticket/:eventId/:attendeeEmail", auth, generateEventTicket);
-router.get("/:eventId", getEvent);
+router.get("/ticket/:eventId/:attendeeEmail", auth, generateEventTicketPDF);
+
 
 module.exports = router;
