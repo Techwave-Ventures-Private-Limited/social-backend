1e2e54d532fb03810d2f2349eda6f09b93aba1ca
kandalkardattu <dattukandalkar143@gmail.com>
2025-07-31 14:15:55 +0530

    route path issue

--- route/eventRoute.js
+++ route/eventRoute.js
@@ -6,10 +6,10 @@ const {auth} = require("../middleware/authMiddleware");
 router.post("/createEvent", auth, createEvent);
 router.post("/createTicket", auth, createTicket);
 router.get("/getUserBookedEvents", auth, getUserBookedEvents);
-router.get("/:eventId", getEvent);
-router.post("/bookTicket", bookTicket);
 router.get("/getAllEvents", getAllEvents);
 router.get("/ticket/:eventId/:attendeeEmail", auth, generateEventTicketPDF);
+router.post("/bookTicket", bookTicket);
+router.get("/:eventId", getEvent);
 
 
 module.exports = router;
