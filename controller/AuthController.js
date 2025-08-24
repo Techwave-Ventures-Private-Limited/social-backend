9589bc2a2772777b756548021bf43aba6162322f
legion <bagulshubham72@gmail.com>
2025-08-24 16:58:07 +0530

    auth fixes

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -116,7 +116,7 @@ exports.login = async(req,res) =>{
               expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
               httpOnly: true,
             };
-            return res.cookie("token", token, options).status(200).json({
+            return res.status(200).json({
               success: true,
               token,
               user,
