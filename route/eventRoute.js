a1b6bdb12d3637bee4e7c47d29bf1365830b0731
legion <bagulshubham72@gmail.com>
2025-09-01 21:22:41 +0530

    ai change

--- route/eventRoute.js
+++ route/eventRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createEvent, createTicket, getEvent, getAllEvents, bookTicket, getUserBookedEvents, generateEventTicketPDF } = require("../controller/EventController");
+const { createEvent, createTicket, getEvent, getAllEvents, bookTicket, getUserBookedEvents, generateEventTicketPDF, getUserEvents, deleteEvent, getAttendesUser } = require("../controller/EventController");
 const router = express.Router();
 const {auth} = require("../middleware/authMiddleware");
 
@@ -10,6 +10,8 @@ router.get("/getAllEvents", getAllEvents);
 router.get("/ticket/:eventId/:attendeeEmail", auth, generateEventTicketPDF);
 router.post("/bookTicket", bookTicket);
 router.get("/:eventId", getEvent);
-
+router.get("/user/self", auth, getUserEvents);
+router.delete("/:eventId", auth, deleteEvent);
+router.get("/attendees/:eventId", auth, getAttendesUser)
 
 module.exports = router;
