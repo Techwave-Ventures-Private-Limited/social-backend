e0fd649740d82a2bba36fd51c797747afeed9d9d
legion <bagulshubham72@gmail.com>
2025-11-08 16:33:25 +0530

    added connection collection

--- modules/connection.js
+++ modules/connection.js
@@ -1,23 +1,24 @@
-const  mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-const connectionSchema  = new mongoose.Schema({
+const connectionSchema = new mongoose.Schema({
+  follower: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+    index: true,
+  },
+  following: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+    index: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-    follower: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        index: true
-    },
-    following: {
-        type : mongoose.Schema.Types.ObjectId,
-        required: true,
-        index: true
-    },
-    createdAt : {
-        type:Date,
-        default: Date.now,
-    },
-})
+connectionSchema.index({ follower: 1, following: 1 }, { unique: true });
 
-connectionSchema.index({ follower: 1, following: 1 });
-
-module.exports =  mongoose.model("Connectino", connectionSchema);
+module.exports = mongoose.model("Follow", connectionSchema);
