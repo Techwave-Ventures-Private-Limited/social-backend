472707226b36f84b62db235b45c5beb261ca8583
akash-0963 <akashjare09@gmail.com>
2025-12-11 15:58:21 +0530

    fix acl error

--- utils/s3Upload.js
+++ utils/s3Upload.js
@@ -19,7 +19,7 @@ const uploadResumeToS3 = async (file, userId) => {
         Key: fileName,
         Body: file.buffer,
         ContentType: file.mimetype,
-        ACL: 'public-read' // Make file publicly accessible
+        // ACL: 'public-read' // Make file publicly accessible
     };
 
     await s3Client.send(new PutObjectCommand(uploadParams));
